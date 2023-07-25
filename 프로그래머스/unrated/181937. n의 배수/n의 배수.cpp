5d49201a2adf5584da0740707c00aedc47338d1c
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2023-07-25 11:40:29 +0900

    [unrated] Title: n의 배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.cpp
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.cpp
@@ -4,10 +4,5 @@
 using namespace std;
 
 int solution(int num, int n) {
-    int answer = 0;
-    if(num % n == 0)
-        answer = 1;
-    else
-        answer = 0;
-    return answer;
+    return num%n==0 ? 1:0;
 }
