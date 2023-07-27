7a05d3543dca44cdf021626d3c489ccfb93969e5
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2023-07-27 13:39:18 +0900

    [level 0] Title: 모음 제거, Time: 0.13 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.cpp
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.cpp
@@ -1,12 +1,11 @@
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
-string solution(string str) {
+string solution(string my_string) {
     string answer = "";
-    for(int i = 0; i < str.size();i++)
-        if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u')
-            answer += str[i];
+    answer = regex_replace(my_string, regex("[aeiou]"), "");
     return answer;
 }
