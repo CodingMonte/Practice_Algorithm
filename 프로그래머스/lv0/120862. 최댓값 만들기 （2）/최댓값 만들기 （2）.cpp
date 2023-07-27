d5b735721ea270d1aa425e2d4c531f00ccade422
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2023-07-27 16:09:41 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = -2147483648;
+    
+    for(int i = 0; i < numbers.size() - 1; i++)
+        for(int j = i + 1; j < numbers.size(); j++)
+            answer = max(answer,numbers[i]*numbers[j]);
+    return answer;
+}
