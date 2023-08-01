a8cdc4fa678362c65b425a03cb0332600a3e43e6
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2023-08-01 13:13:40 +0900

    [level 0] Title: 안전지대, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board) {
+    int answer = 0;
+    for(int i = 0; i < board.size(); i++)
+    {
+        for(int j = 0; j < board[0].size();j++)
+        {
+            if(board[i][j] == 1)
+            {
+                for(int x = i - 1; x <= i + 1;x++)
+                {
+                    for(int y = j - 1; y <= j + 1; y++)
+                    {
+                        if(board[x][y] == 0 && x < board.size() && y < board[0].size())
+                            board[x][y] = 2;
+                    }
+                }
+            }
+        }
+    }
+    for(int i = 0; i < board.size(); i++)
+        for(int j = 0; j < board[0].size();j++)
+            if(board[i][j] == 0)
+                answer++;
+    return answer;
+}
