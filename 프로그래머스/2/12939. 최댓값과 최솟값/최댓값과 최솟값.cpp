127cc5d68750c191eacfa5bece674d7de0da91d9
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2023-11-22 02:57:08 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -7,15 +7,16 @@ using namespace std;
 
 string solution(string s) {
     string answer = "";
-    stringstream ss(s);
-    int n;
+    stringstream ss(s);     // stringstream 선언
+    int n;                  // stream에서 데이터를 받을 놈들
     vector<int> num;
-    
-    while(ss >> n)
+    while(ss >> n)          // 위에서 말했던 while 문!!
         num.push_back(n);
+    
+    // 최댓값과 최솟값이 담긴 iterator 추출
     auto Max = max_element(num.begin(), num.end());
     auto Min = min_element(num.begin(), num.end());
-    
+    // 포인터를 사용하여 iterator에 있는 값을 answer에 저장
     answer = to_string(*Min) + " " + to_string(*Max);
     
     return answer;
