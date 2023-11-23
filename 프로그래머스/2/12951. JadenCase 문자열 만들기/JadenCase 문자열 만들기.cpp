2be685a848b2089c44549442147686b8cc0d3dcc
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2023-11-24 01:32:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -1,16 +1,13 @@
 #include <string>
 #include <vector>
-#include <cctype>
 
 using namespace std;
 
 string solution(string s) {
-    
-    if(isalpha(s[0]))
-        s[0] = toupper(s[0]);
+    s[0] = toupper(s[0]);
     for(int i = 1; i < s.size();i++)
     {
-        if(isalpha(s[i]) && s[i-1] == ' ')
+        if(s[i-1] == ' ')
             s[i] = toupper(s[i]);
         else
             s[i] = tolower(s[i]);
