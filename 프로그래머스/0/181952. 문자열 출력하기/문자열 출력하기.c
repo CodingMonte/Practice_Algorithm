ebdabdab5b8d1db72fe093037a63c8bbdcd0308c
Sunho Choi <99657809+CodingMonte@users.noreply.github.com>
2024-05-15 21:15:18 +0900

    [level 0] Title: 문자열 출력하기, Time: 1.94 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000001];
+    
+    // 문자열 입력 받기
+    scanf("%s", str);
+    
+    // 입력 받은 문자열 출력
+    printf("%s\n", str);
+    
+    return 0;
+}
